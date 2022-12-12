9c4f006faedeec07528529b7035e06dd8997efb3
ptk-trindade <PatrickTrindade@poli.ufrj.br>
2022-12-11 22:33:00 -0300

    Runs FordFulkerson 1 and 2 for every graph 10x

--- tests.go
+++ tests.go
@@ -13,16 +13,7 @@ func tests() { //adjacency [][]*Edge
 	// test 1: Calculate the distance between two nodes
 	log.Println("\n --- STARTING TESTS ---")
 	test_files := []string{"grafo_rf_1.txt", "grafo_rf_2.txt", "grafo_rf_3.txt", "grafo_rf_4.txt", "grafo_rf_5.txt", "grafo_rf_6.txt", "grafo_rf_7.txt", "grafo_rf_8.txt"}
-	flows := [][]float64{
-		{0, 27, 0, 21, 25, 25, 30, 56, 15, 20},
-		{0, 12178, 0, 13646, 14698, 19796, 1944, 12846, 11225, 18582},
-		{0, 46, 0, 32, 19, 31, 39, 10, 27, 35},
-		{0, 9623, 0, 14415, 27256, 28358, 28071, 17701, 15298, 23563},
-		{0, 12, 0, 39, 28, 44, 34, 42, 40, 54},
-		{0, 28960, 0, 52777, 39931, 30261, 60146, 47395, 66609, 39032},
-		{0, 64, 0, 80, 23, 60, 37, 24, 30, 25},
-		{0, 462474, 0, 494372, 390674, 187092, 254688, 198591, 566312, 233418},
-	}
+	run_n_times := 10
 	for case_i, filename := range test_files {
 		fmt.Println("--- Case", case_i+1, " ---")
 		lenVertex, degrees, edges := readInitFile(filename)
@@ -50,43 +41,40 @@ func tests() { //adjacency [][]*Edge
 		}
 
 		// VERSION 1: (1 bfs and no parallelism)
+		times_list := make([]time.Duration, run_n_times)
+		var maxFlow float64
 		ford_fulkerson_time := time.Now()
-		fmt.Print("Time per ff: ")
-		for i := 0; i < 10; i++ {
+		for i := 0; i < run_n_times; i++ {
 			ff_time := time.Now()
-			maxFlow, _ := fordFulkersonV2(adjacency, uint32(i), uint32(i*10))
-			if maxFlow != flows[case_i][i] {
-				fmt.Println("ERROR: flow", maxFlow, "!= expected", flows[case_i][i])
-			}
-			fmt.Print("\t", maxFlow, "(", time.Since(ff_time), ")")
+			maxFlow, _ = fordFulkerson(adjacency, 1, 2)
+			// if maxFlow != flows[case_i][i] {
+			// 	fmt.Println("ERROR: flow", maxFlow, "!= expected", flows[case_i][i])
+			// }
+			times_list[i] = time.Since(ff_time)
 		}
-		fmt.Println("\nTotal time V1:", time.Since(ford_fulkerson_time))
+		total_time := time.Since(ford_fulkerson_time)
+		fmt.Print("maxFlow", maxFlow, " >\t")
+		for _, t := range times_list {
+			fmt.Print("\t", t)
+		}
+		fmt.Println("\nTotal time V1:", total_time)
 
 		// VERSION 2: (2 bfs and no parallelism)
 		ford_fulkerson_time = time.Now()
-		fmt.Print("Time per ff: ")
-		for i := 0; i < 10; i++ {
+		for i := 0; i < run_n_times; i++ {
 			ff_time := time.Now()
-			maxFlow, _ := fordFulkersonV2(adjacency, uint32(i), uint32(i*10))
-			if maxFlow != flows[case_i][i] {
-				fmt.Println("ERROR: flow", maxFlow, "!= expected", flows[case_i][i])
-			}
-			fmt.Print("\t", maxFlow, "(", time.Since(ff_time), ")")
+			maxFlow, _ = fordFulkersonV2(adjacency, 1, 2)
+			// if maxFlow != flows[case_i][i] {
+			// 	fmt.Println("ERROR: flow", maxFlow, "!= expected", flows[case_i][i])
+			// }
+			times_list[i] = time.Since(ff_time)
 		}
-		fmt.Println("\nTotal time V2:", time.Since(ford_fulkerson_time))
-
-		// VERSION 3: (2 bfs and parallelism)
-		ford_fulkerson_time = time.Now()
-		fmt.Print("Time per ff: ")
-		for i := 0; i < 10; i++ {
-			ff_time := time.Now()
-			maxFlow, _ := fordFulkersonV3(adjacency, uint32(i), uint32(i*10))
-			if maxFlow != flows[case_i][i] {
-				fmt.Println("ERROR V3: flow", maxFlow, "!= expected", flows[case_i][i])
-			}
-			fmt.Print("\t", maxFlow, "(", time.Since(ff_time), ")")
+		total_time = time.Since(ford_fulkerson_time)
+		fmt.Print("maxFlow", maxFlow, " >\t")
+		for _, t := range times_list {
+			fmt.Print("\t", t)
 		}
-		fmt.Println("\nTotal time V3:", time.Since(ford_fulkerson_time))
+		fmt.Println("\nTotal time V2:", total_time)
 	}
 	fmt.Println(" --- END --- ")
 }
